e35d27f1a764db50d1c891bc4da610d9b4f5be83
gibran <gibranayala798@gmail.com>
2021-06-21 07:55:52 -0500

    Add fast scrolling and hlsearch

--- lua/keymappings/init.lua
+++ lua/keymappings/init.lua
@@ -3,5 +3,12 @@
 
 vim.api.nvim_set_keymap('n', ';q', ':q<CR>', {silent=true})
 vim.api.nvim_set_keymap('n', ';w', ':w<CR>', {silent=true})
+
 vim.api.nvim_set_keymap('n', '<space>e', ':NERDTreeFind<CR>', {silent=true})
 vim.api.nvim_set_keymap('n', ';e', ':edit .<CR>', {silent=true})
+
+vim.api.nvim_set_keymap('n', '<space>h', ':set hlsearch!', {silent=true})
+
+vim.api.nvim_set_keymap('n', '<C-j>', 'j10', {silent=true})
+vim.api.nvim_set_keymap('n', '<C-k>', 'k10', {silent=true})
+
